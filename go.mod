2a186003f7ae51333a76d637e5d37e43e7d93818
imhoss <max_at_home@protonmail.com>
2021-08-02 16:35:44 -0400

    testing cloud run cd

---
+++ go.mod
@@ -0,0 +1,9 @@
+module example.com/gobot.go
+
+go 1.16
+
+require (
+	cloud.google.com/go v0.89.0
+	github.com/bwmarrin/discordgo v0.23.2
+	google.golang.org/genproto v0.0.0-20210729151513-df9385d47c1b
+)
---
+++ gobot.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"github.com/bwmarrin/discordgo"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
+)
+
+// Variables used for command line parameters
+//var (
+//	Token string
+//)
+
+//func init() {
+
+//	flag.StringVar(&Token, "t", "", "Bot Token")
+//	flag.Parse()
+//}
+
+// const Token string = "ODY4Njg1MTk2OTk4ODc3MjI1.YPzQIg.3yPfIg9xtDwmu6tfgmE-bNmmf5E"
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := ""
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", name)
+}
+
+func defaultServer() {
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+//var BotID string
+func accessSecretVersion() (string, error) {
+	// name := "projects/my-project/secrets/my-secret/versions/5"
+	name := "projects/scratch-project-321714/secrets/bot-token/versions/latest"
+
+	// Create the client.
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		return name, fmt.Errorf("failed to create secretmanager client: %v", err)
+	}
+	defer client.Close()
+
+	// Build the request.
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: name,
+	}
+
+	// Call the API.
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		return name, fmt.Errorf("failed to access secret version: %v", err)
+	}
+
+	// WARNING: Do not print the secret in a production environment - this snippet
+	// is showing how to access the secret material.
+	// fmt.Fprintf(w, "Plaintext: %s\n", string(result.Payload.Data))
+
+	secret := string(result.Payload.Data)
+	return secret, nil
+}
+
+func main() {
+	// log.Print("starting server...")
+	// http.HandleFunc("/", handler)
+
+	// // Determine port for HTTP service.
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = "8080"
+	// 	log.Printf("defaulting to port %s", port)
+	// }
+
+	// // Start HTTP server.
+	// log.Printf("listening on port %s", port)
+	// if err := http.ListenAndServe(":"+port, nil); err != nil {
+	// 	log.Fatal(err)
+	// }
+	go defaultServer()
+
+	// GCP project in which to store secrets in Secret Manager.
+	// Create the client.
+	Token, _ := accessSecretVersion()
+	// fmt.Println(Token)
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+
+	if m.Content == "thanks" {
+		s.ChannelMessageSend(m.ChannelID, "np. love you")
+	}
+
+	if m.Content == "List on!" {
+		s.ChannelMessageSend(m.ChannelID, "<------ TODO LIST for shelny-------->\n    #1 jack of \n <-----end list ------>")
+	}
+}
